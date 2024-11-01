a6e060c1a89561106a32d66f3ec94ba58776b4c6
lll <798799155@qq.com>
2024-11-01 22:26:23 +0800

    fix: 20241101

--- src/EnemyMovement.js
+++ src/EnemyMovement.js
@@ -18,6 +18,9 @@ let currentFrame = 0
 class Enemy {
   constructor () {
     this.flyingSpeed = Math.random() - 0.8;
+    this.flyingAngle = 0;
+    this.flyingAngleSpeed = Math.random() * 2 + 0.5;
+    this.curve = Math.random() * 100;
 
     this.role = new CreateRoleAnimation(enemyAnimationInfo);
     this.roleFrameXPos = 0;
@@ -32,11 +35,19 @@ class Enemy {
     // this.x += this.flyingSpeed;
     // this.y += this.flyingSpeed;
 
-    // 第二种上下做sin函数的飞行
-    this.x += this.flyingSpeed;
-    if (this.x + this.role.roleWidth < 0) {
-      this.x = CANVAS_WIDTH;
-    }
+    // 第二种：从右往左飞行，且上下做sin函数的飞行
+    // this.x += this.flyingSpeed;
+    // if (this.x + this.role.roleWidth < 0) {
+    //   this.x = CANVAS_WIDTH;
+    // }
+    // this.y = this.curve * (Math.sin(this.flyingAngle) * 10);
+    // this.flyingAngle += this.flyingAngleSpeed;
+
+
+    // 第三种：旋转飞行，pi除以的数决定运动轨迹
+    this.x = this.curve * Math.cos(this.flyingAngle * Math.PI / 180) + (CANVAS_WIDTH / 2 - this.role.roleWidth / 2)
+    this.y = this.curve * Math.sin(this.flyingAngle * Math.PI / 180) + (CANVAS_WIDTH / 2 - this.role.roleWidth / 2)
+    this.flyingAngle += this.flyingAngleSpeed;
 
 
     const [roleFrameXPos, roleFrameYPos] = this.role.updateRoleFrameIndex('move_f', currentFrame);
@@ -46,7 +57,6 @@ class Enemy {
   }
   draw() {
     this.role.drawRole(ctx, enemyImage, this.roleFrameXPos, this.roleFrameYPos, this.x, this.y)
-    // ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 }
 
